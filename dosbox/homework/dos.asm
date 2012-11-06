b8b57c84fbe08c20541fc2dc8ec88dde54b04a0b
Dejan Pekter <deximat@gmail.com>
2012-11-06 06:06:57 +0100

    Added missing files

---
+++ dosbox/homework/dos.asm
@@ -0,0 +1,102 @@
+segment .code 
+
+install_dos_handler:
+  pusha
+  push ds
+  push es
+  cli
+  ;writing keyboard buffer to file when dos is idle
+  ;we will do this using int 28h
+  
+  ;initialising ds - had a lot of problems with this, just to be sure
+  mov al, 'b'
+  call _printc
+ 
+  mov ax, cs
+  mov ds, ax
+  ;zero
+  xor ax, ax
+  ;putting es to 0
+  mov es, ax
+  ;saving old offset
+  mov ax, [es:28h*4]
+  mov [old_28h_offset], ax
+  ;saving old segment
+  mov ax, [es:28h*4 + 2]
+  mov [old_28h_segment], ax
+
+  ;installing new
+  mov [es:28h*4], word dos_handler
+  mov [es:28h*4+2], cs
+  pop es
+  pop ds
+  popa
+  sti 
+  ret
+uninstall_dos_handler:
+  pusha
+  push ds
+  push es
+  cli
+
+  call set_ds_to_tsr
+
+  ;zero
+  xor ax, ax
+  ;putting es to 0
+  mov es, ax
+  ;restoring old
+  ;offset
+  mov ax, [old_28h_offset]
+  mov [es:28h*4], ax
+  ;segment
+  mov ax, [old_28h_segment]
+  mov [es:28h*4+2], ax
+  pop es
+  pop ds
+  popa
+  sti
+  ret
+
+dos_handler:
+  pusha
+  push ds
+  push es
+  mov al, 'A'
+  call _printc
+  ;initialising ds - had a lot of problems with this, just to be sure
+  mov ax, cs
+  mov ds, ax
+  ;if there is no need to write skip everything
+  cmp [int_28h_flag], byte 1h
+  jne .skip_write
+  ;clear flag
+  mov [int_28h_flag], byte 0h
+  ;check DOS to see if it is safe to use DOS int
+  mov ah, 34h
+  int 21h
+  ;now we have flags adress inside ES:BX
+  xor ax, ax ; setting to zero
+  cmp al, [es:bx]
+  jnz .skip_write ; if this is different from zero then DOS is busy 
+.success:
+  ;write to file
+  ;DS:DX - buffer location
+  ;CX - number of bytes to write to file
+  ;BX - file handle
+  call _write_to_file
+.skip_write:
+  ;calling old int
+  pushf
+  call far word [old_28h]
+  pop es
+  pop ds
+  popa
+  iret
+
+
+old_28h:
+old_28h_offset: dw 0
+old_28h_segment: dw 0
+second: db 0
+int_28h_flag: db 0
---
+++ dosbox/homework/wait.asm
@@ -0,0 +1,30 @@
+org 100h 
+
+segment .code
+
+int 28h
+ret
+mov ah, 01h
+int 21h
+
+mov ah, 01h
+int 21h
+
+
+mov ah, 01h
+int 21h
+
+
+mov ah, 01h
+int 21h
+
+
+mov ah, 01h
+int 21h
+
+
+mov ah, 01h
+int 21h
+ret
+
+
